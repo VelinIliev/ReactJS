a37ecd85bd581fdef3d35217a9c93bc108e73b89
VelinIliev <veliniliev@gmail.com>
2023-08-15 15:14:14 +0300

    Intro to React and JSX

--- 02-first-react-app/src/App.js
+++ 02-first-react-app/src/App.js
@@ -7,15 +7,15 @@ import Link from './components/Link';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Logo />
-        <Paragraph />
-        <Link />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Logo />
+                <Paragraph />
+                <Link />
+            </header>
+        </div>
+    );
 }
 
 export default App;
