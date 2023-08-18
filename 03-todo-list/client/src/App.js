6a93a6b56f44d4bfad9d54ff6b5374708edd1f62
VelinIliev <veliniliev@gmail.com>
2023-08-18 10:48:00 +0300

    Components: Deep Dive

--- 03-todo-list/client/src/App.js
+++ 03-todo-list/client/src/App.js
@@ -9,54 +9,47 @@ import React from 'react';
 
 function App() {
     const [todos, setTodos] = React.useState([]);
+    const [isLoading , setIsLoading] = React.useState(true)
 
     React.useEffect(() => {
         fetch('http://localhost:3030/jsonstore/todos')
             .then(res => res.json())
             .then(data => {
-                // console.log(data);
                 setTodos(Object.values(data))
+                setIsLoading(false)
             })
     }, []);
 
     const toggleStatus = (id) => {
-        // let currentTodo = todos.filter(x => x._id === id)
-        // console.log(todos);
-        setTodos(state => state.map(todo => todo._id === id ? ({ ...todo, isCompleted: !todo.isCompleted }) : todo))
-        // console.log(todos.filter(x => x._id === id));
+        setTodos(state => state.map(todo => todo._id === id 
+            ? ({ ...todo, isCompleted: !todo.isCompleted }) 
+            : todo)
+            )
     }
 
     const addTodo = () => {
         const newId = `todo_${Number(todos[todos.length - 1]._id.split("_")[1]) + 1}`;
         const title = prompt("New task:");
-        // console.log(title);
-        // console.log(newId);
-        setTodos(state => [ ...state , {_id: newId, text: title, isCompleted: false}])
+        setTodos(state => [...state, { _id: newId, text: title, isCompleted: false }])
     }
 
     return (
         <div>
             <Header />
-
             <main className="main">
-
                 <section className="todo-list-container">
                     <h1>Todo List</h1>
-
                     <div className="add-btn-container">
                         <button className="btn" onClick={addTodo}>+ Add new Todo</button>
                     </div>
-
                     <div className="table-wrapper">
-
-                        {/* <Loading /> */}
-
-                        <Todos todos={todos} toggleStatus={toggleStatus} />
+                        {isLoading 
+                            ? <Loading /> 
+                            : <Todos todos={todos} toggleStatus={toggleStatus} />}
+                        
                     </div>
                 </section>
             </main>
-
-            {/* <!-- Footer --> */}
             <Footer />
         </div>
     );
--- 03-todo-list/client/src/components/Todo.js
+++ 03-todo-list/client/src/components/Todo.js
@@ -5,7 +5,9 @@ const Todo = function ({ todo, toggleStatus}) {
             <td>{todo.text}</td>
             <td>{todo.isCompleted ? "Compleated" : "Incomplete"}</td>
             <td className="todo-action">
-                <button className="btn todo-btn" onClick= { () => toggleStatus(todo._id)}>Change status</button>
+                <button className="btn todo-btn" onClick= { () => toggleStatus(todo._id)}>
+                    Change status
+                    </button>
             </td>
         </tr>
     )
